41c282c0c1f95c3b4f9e21dd8e642ba2d496eebf
gastontecheradev <acorde.musica10@gmail.com>
2026-01-31 21:59:59 -0300

    Agregar archivos de proyecto.

---
+++ Biblioteca_ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Biblioteca;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Libro Metafisica = new Libro("Metafísica", "Filosofía", 5, true);
+
+        Metafisica.mostrarInfo();
+    }
+
+    // Clase
+    public class Libro
+    {
+        // Atributos
+        private string titulo;
+        private string categoria;
+        private int indice;
+        private bool disponible;
+
+        // Metodos
+        public void mostrarInfo()
+        {
+            Console.WriteLine("Titulo: " + titulo);
+            Console.WriteLine("Categoria: " + categoria);
+            Console.WriteLine("Indice: " + indice);
+
+            if (disponible)
+            {
+                Console.WriteLine("Está disponible");
+            }
+            else
+            {
+                Console.WriteLine("No está disponible");
+            }
+        }
+
+        // Constructor
+        public Libro(string elTitulo, string laCategoria, int elIndice, bool estadoDisponible)
+        {
+            titulo = elTitulo;
+            categoria = laCategoria;
+            indice = elIndice;
+            disponible = estadoDisponible;
+        }
+
+    }
+}
