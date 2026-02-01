f0f173f70d6869d317e85e4582dc9f95b5ea7bf5
gastontecheradev <acorde.musica10@gmail.com>
2026-01-31 23:51:50 -0300

    abc

---
+++ Biblioteca_ConsoleApp/Libro.cs
@@ -0,0 +1,47 @@
+﻿namespace Biblioteca_ConsoleApp
+{
+    public class Libro : ILibro
+    {
+        // Atributos
+        private string titulo;
+        private string categoria;
+        private int indice;
+        private bool disponible;
+
+        // Metodos
+        public void MostrarInfo()
+        {
+            Console.WriteLine("Titulo: " + titulo);
+            Console.WriteLine("Categoria: " + categoria);
+            Console.WriteLine("Indice: " + indice);
+
+            if (disponible)
+            {
+                Console.WriteLine("Está disponible");
+            }
+            else
+            {
+                Console.WriteLine("No está disponible");
+            }
+        }
+
+        public void MostrarTitulo()
+        {
+            Console.WriteLine(titulo);
+        }
+
+        // Constructor
+        public Libro(string elTitulo, string laCategoria, int elIndice, bool estadoDisponible)
+        {
+            titulo = elTitulo;
+            categoria = laCategoria;
+            indice = elIndice;
+            disponible = estadoDisponible;
+        }
+    }
+
+    interface ILibro
+    {
+        void MostrarInfo();
+    }
+}
--- Biblioteca_ConsoleApp/Program.cs
+++ Biblioteca_ConsoleApp/Program.cs
@@ -1,51 +1,49 @@
-﻿using System;
-using System.Linq;
-
-namespace Biblioteca;
+﻿namespace Biblioteca_ConsoleApp;
 
 public static class Program
 {
     public static void Main()
     {
-        Libro Metafisica = new Libro("Metafísica", "Filosofía", 5, true);
+        Libro Metafisica = new Libro("Metafisica", "Filosofia", 5, true);
+        Libro Renacimiento = new Libro("Renacimiento", "Historia", 5, true);
+        Libro Barroco = new Libro("Metafisica", "Música", 5, true);
 
-        Metafisica.mostrarInfo();
+        Libro[] listaLibros = { Metafisica, Renacimiento, Barroco };
+
+        int opcion = 10;
+
+        do
+        {
+            Console.WriteLine("--- BIBLIOTECA ---\n");
+            Console.WriteLine("Elija una opción:\n");
+            Console.WriteLine("1.Mostrar Libros");
+            Console.WriteLine("0.Salir");
+
+            try
+            {
+                opcion = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ingrese un número válido. Presione una tecla para continuar.");
+            }
+
+            if (opcion == 1)
+            {
+                Console.WriteLine("--- Lista de libros ---\n");
+
+                for (int i = 0; i < listaLibros.Length; i++)
+                {
+                    listaLibros[i].MostrarTitulo();
+                    opcion = 10;
+                }
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+        while (opcion != 0);
     }
 
-    // Clase
-    public class Libro
-    {
-        // Atributos
-        private string titulo;
-        private string categoria;
-        private int indice;
-        private bool disponible;
 
-        // Metodos
-        public void mostrarInfo()
-        {
-            Console.WriteLine("Titulo: " + titulo);
-            Console.WriteLine("Categoria: " + categoria);
-            Console.WriteLine("Indice: " + indice);
-
-            if (disponible)
-            {
-                Console.WriteLine("Está disponible");
-            }
-            else
-            {
-                Console.WriteLine("No está disponible");
-            }
-        }
-
-        // Constructor
-        public Libro(string elTitulo, string laCategoria, int elIndice, bool estadoDisponible)
-        {
-            titulo = elTitulo;
-            categoria = laCategoria;
-            indice = elIndice;
-            disponible = estadoDisponible;
-        }
-
-    }
 }
