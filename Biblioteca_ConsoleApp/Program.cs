0bf1cd1fd6b03ce1e6db25c91e3dec8f4d5146a8
gastontecheradev <acorde.musica10@gmail.com>
2026-02-04 21:23:28 -0300

    abc

--- Biblioteca_ConsoleApp/Program.cs
+++ Biblioteca_ConsoleApp/Program.cs
@@ -1,14 +1,20 @@
-﻿namespace Biblioteca_ConsoleApp;
+﻿using System.Collections.Generic;
 
-public static class Program
+namespace Biblioteca_ConsoleApp;
+
+public  class Program
 {
     public static void Main()
     {
-        Libro Metafisica = new Libro("Metafisica", "Filosofia", 5, true);
-        Libro Renacimiento = new Libro("Renacimiento", "Historia", 5, true);
-        Libro Barroco = new Libro("Metafisica", "Música", 5, true);
+        Libro Metafisica = new Libro("Metafisica", "Filosofia", 1, true);
+        Libro Renacimiento = new Libro("Renacimiento", "Historia", 2, true);
+        Libro Barroco = new Libro("Metafisica", "Música", 3, true);
 
-        Libro[] listaLibros = { Metafisica, Renacimiento, Barroco };
+        LinkedList<Libro> listaLibros = new LinkedList<Libro>();
+
+        listaLibros.AddLast(Metafisica);
+        listaLibros.AddLast(Renacimiento);
+        listaLibros.AddLast(Barroco);
 
         int opcion = 10;
 
@@ -16,7 +22,9 @@ public static class Program
         {
             Console.WriteLine("--- BIBLIOTECA ---\n");
             Console.WriteLine("Elija una opción:\n");
-            Console.WriteLine("1.Mostrar Libros");
+            Console.WriteLine("1.Mostrar Libros\n");
+            Console.WriteLine("2.Agregar Libro\n");
+            Console.WriteLine("3.Eliminar Libro\n");
             Console.WriteLine("0.Salir");
 
             try
@@ -28,22 +36,55 @@ public static class Program
                 Console.WriteLine("Ingrese un número válido. Presione una tecla para continuar.");
             }
 
-            if (opcion == 1)
+            switch(opcion)
             {
-                Console.WriteLine("--- Lista de libros ---\n");
+                case 1:
+                    Console.WriteLine("--- Lista de libros ---\n");
 
-                for (int i = 0; i < listaLibros.Length; i++)
-                {
-                    listaLibros[i].MostrarTitulo();
-                    opcion = 10;
-                }
+                    foreach (Libro n in listaLibros)
+                    {
+                        n.MostrarTitulo();
+                    }
+                    break;
+                case 2:
+                    Console.Write("Ingrese el título del libro: \n");
+                    string nombreIngresado = Console.ReadLine();
+
+                    Console.Write("Ingrese el género del libro: \n");
+                    string generoIngresado = Console.ReadLine();
+
+                    Console.Write("Ingrese el índice del libro: \n");
+                    int indiceIngresado = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Indique si el libro está disponible (si / no): \n");
+                    string disponibleString = Console.ReadLine();
+
+                    bool disponibleBool = false;
+
+                    if (disponibleString == "si")
+                    {
+                        disponibleBool = true;
+                    }
+
+                    Libro libroIngresado = new Libro(nombreIngresado, generoIngresado, indiceIngresado, disponibleBool);
+
+                    listaLibros.AddLast(libroIngresado);
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    Console.WriteLine();
+                    break;
+
+
             }
+
+            
 
             Console.ReadKey();
             Console.Clear();
         }
         while (opcion != 0);
     }
-
-
 }
